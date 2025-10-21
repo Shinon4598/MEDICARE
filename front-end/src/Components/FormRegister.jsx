e08c9d89ca9407f1461f986bb0a365c0726ce70d
Shinon4598 <alguien4598@gmail.com>
2025-10-21 01:01:05 -0300

    agregue iconos a los inputs

--- front-end/src/Components/FormRegister.jsx
+++ front-end/src/Components/FormRegister.jsx
@@ -1,26 +1,29 @@
 import { Link } from "wouter";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faLock,
+  faFingerprint,
+  faAddressCard,
+  faFaceSmile,
+  faPhone,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import Input from "./Input";
 export default function FormRegister() {
   return (
-    <form className="flex flex-col gap-5 w-[80%]" action="submit">
+    <form className="flex flex-col gap-5  p-5" action="submit">
       <h2 className="text-4xl text-gray-600 font-bold text-center">
         Registrate
       </h2>
-      <section className="md:grid md:grid-cols-2 md:place-items-center flex flex-col justify-center items-center w-full p-2">
-        <Input texto="Nombre/s" tipo="text" icono={faLock}></Input>
-        <Input texto="Apellido" tipo="text" icono={faLock}></Input>
+      <section className="md:grid md:grid-cols-2 md:place-items-center gap-3 flex flex-col justify-center items-center w-full p-2">
+        <Input texto="Nombre/s" tipo="text" icono={faPerson}></Input>
+        <Input texto="Apellido" tipo="text" icono={faPerson}></Input>
+        <Input texto="DNI" tipo="number" icono={faFingerprint}></Input>
+        <Input texto="Obra Social" tipo="select" icono={faAddressCard}></Input>
+        <Input texto="Numero celular" tipo="number" icono={faPhone}></Input>
         <Input texto="Email" icono={faUser}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
-        <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
         <Input texto="Contraseña" tipo="password" icono={faLock}></Input>
         <Input
           texto="Confirmar contraseña"
