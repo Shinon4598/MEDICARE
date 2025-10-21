e8b53445028faa18ec16927bb1e7d4098a82f43e
Shinon4598 <alguien4598@gmail.com>
2025-10-21 16:40:38 -0300

    agregando rhf

--- front-end/src/Components/FormRegister.jsx
+++ front-end/src/Components/FormRegister.jsx
@@ -2,6 +2,7 @@ import * as z from "zod";
 import Select from "./Select";
 import Input from "./Input";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   faUser,
   faLock,
@@ -12,96 +13,139 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
 export default function FormRegister() {
+  const schema = z
+    .object({
+      nombre: z
+        .string("Debe ser una cadena de texto")
+        .min(3, "Ingrese minimo 3 caracteres")
+        .max(30, "El máximo de caracteres es de 30"),
+      apellido: z
+        .string("Debe ser una cadena de texto")
+        .min(3, "Ingrese minimo 3 caracteres")
+        .max(20, "El máximo de caracteres es de 20"),
+      dni: z.coerce.number("Debe ser un número").min(7, "minimo 7"),
+      celular: z.string().regex(/^\+54\s?9?\s?\d{2}\s?\d{4}\s?\d{4}$/, {
+        message: "Formato inválido. Usa el formato +54 9 11 1234 5678",
+      }),
+      email: z
+        .email("formato invalido")
+        .nonempty("Este campo no debe estar vacío"),
+      obra_social: z
+        .string("Debe ser una cadena de texto")
+        .refine((value) => value !== "", {
+          message: "Debe seleccionar una opción válida",
+        }),
+      contraseña: z
+        .string("Debe ser una cadena de texto")
+        .nonempty("Este campo no debe estar vacío")
+        .min(8, "Minimo de 8 caracteres")
+        .regex(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/,
+          "Mínimo 8 caracteres, mayúscula, minúscula, número y símbolo"
+        ),
+      confirmar_contraseña: z
+        .string("Debe ser una cadena de texto")
+        .nonempty("Este campo no debe estar vacío"),
+    })
+    .refine((data) => data.contraseña === data.confirmar_contraseña, {
+      message: "Las contraseñas no coinciden",
+      path: ["confirmar_contraseña"],
+    });
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
-  const validation = z.object({
-    nombre: z
-      .string()
-      .min(3, "Ingrese minimo 3 caracteres")
-      .max(30, "El máximo de caracteres es de 30"),
-    apellido: z
-      .string()
-      .min(3, "Ingrese minimo 3 caracteres")
-      .max(20, "El máximo de caracteres es de 20"),
-    dni: z.coerce
-      .number()
-      .min(7, "formato invalido")
-      .max(8, "formato invalido"),
-    celular: z.string().regex(/^\+54\s?9?\s?\d{2}\s?\d{4}\s?\d{4}$/, {
-      message: "Formato inválido. Usa el formato +54 9 11 1234 5678",
-    }),
-    email: z.email({
-      error: (iss) =>
-        iss.input == undefined ? "El campo es requerido" : "Entrada invalida",
-    }),
-    obra_social: z.string().nonempty("Seleccione un valor"),
-    password: z
-      .string()
-      .min(8, "Minimo de 8 caracteres")
-      .regex(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/,
-        "Mínimo 8 caracteres, mayúscula, minúscula, número y símbolo"
-      ),
-    confirmPassword: z
-      .string()
-      .refine((v) => v == password, { error: "Las contraseñas no coinciden" }),
-  });
+  } = useForm({ resolver: zodResolver(schema) });
+
   const onSubmit = (data) => console.log(data);
+
   return (
-    <form className="flex flex-col gap-5  p-5" action="submit">
+    <form
+      className="flex flex-col gap-5  p-5"
+      action="submit"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <h2 className="text-4xl text-gray-600 font-bold text-center">
         Registrate
       </h2>
       <section className="md:grid md:grid-cols-2 md:place-items-center gap-3 flex flex-col justify-center items-center w-full p-2">
         <Input
-          texto="Nombre/s"
+          label="Nombre/s"
           name="nombre"
           tipo="text"
           icono={faPerson}
+          register={register}
+          error={errors?.nombre?.message}
         ></Input>
         <Input
-          texto="Apellido"
+          label="Apellido"
           name="apellido"
           tipo="text"
           icono={faPerson}
+          register={register}
+          error={errors?.apellido?.message}
         ></Input>
+
         <Input
-          texto="DNI"
+          label="DNI"
           name="dni"
           tipo="number"
           icono={faFingerprint}
+          register={register}
+          error={errors?.dni?.message}
         ></Input>
+        {errors?.dni?.message}
+
         <Select
-          texto="Obra Social"
+          label="Obra Social"
           name="obra_social"
           tipo="select"
           icono={faAddressCard}
+          register={register}
+          error={errors?.obra_social?.message}
         >
           <option value="">Obra social / particular</option>
+          <option value="p">Particular</option>
         </Select>
+
         <Input
-          texto="Numero celular"
+          label="Numero celular"
           name="celular"
           tipo="number"
           icono={faPhone}
+          register={register}
+          error={errors?.celular?.message}
         ></Input>
-        <Input texto="Email" name="email" icono={faUser}></Input>
+        {errors?.celular?.message}
+
         <Input
-          texto="Contraseña"
+          label="Email"
+          name="email"
+          icono={faUser}
+          register={register}
+          error={errors?.email?.message}
+        ></Input>
+
+        <Input
+          label="Contraseña"
           name="contraseña"
           tipo="password"
           icono={faLock}
+          register={register}
+          error={errors?.contraseña?.message}
         ></Input>
+        {errors?.contraseña?.message}
+
         <Input
-          texto="Confirmar contraseña"
+          label="Confirmar contraseña"
           name="confirmar_contraseña"
           tipo="password"
           icono={faLock}
+          register={register}
+          error={errors?.confirmar_contraseña?.message}
         ></Input>
+        {errors?.confirmar_contraseña?.message}
       </section>
 
       <button
--- front-end/src/Components/Input.jsx
+++ front-end/src/Components/Input.jsx
@@ -4,18 +4,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useId } from "react";
 
 function Input({
-  texto = "default",
+  label = "default",
   name = "",
   icono,
   tipo = "text",
-  children,
+  register,
+  error,
 }) {
-  const [valueLabel, setValueLabel] = useState(texto);
+  const [valueLabel, setValueLabel] = useState(label);
   const id = useId();
   const idPersonalizado = `${id}_${name
     .trim()
     .toLowerCase()
     .replace(" ", "_")}`;
+
+  // Verificar que register existe y es una función
+  const registerProps = register ? register(name) : {};
   return (
     <div className="relative">
       <label
@@ -27,14 +31,16 @@ function Input({
         {valueLabel}
       </label>
       <input
+        {...registerProps}
         onChange={(e) => {
-          !!e.target.value ? setValueLabel("") : setValueLabel(texto);
+          !!e.target.value ? setValueLabel("") : setValueLabel(label);
         }}
         className="border border-gray-300 rounded-[15px] py-2 pl-10 shadow"
         type={tipo}
         name={name}
         id={idPersonalizado}
       />
+      {error && <p className="text-red-900">{error}</p>}
     </div>
   );
 }
